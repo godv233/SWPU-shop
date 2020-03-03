61a0a1dc0c8685214827337ead634c377de84859
godv233 <zengwei233@126.com>
2020-03-03 10:33:52 +0800

    Initial commit

---
+++ shop-flash/src/main/java/com/swpu/shop/shopflash/ShopFlashApplication.java
@@ -0,0 +1,13 @@
+package com.swpu.shop.shopflash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopFlashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopFlashApplication.class, args);
+    }
+
+}
