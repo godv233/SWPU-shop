0913260608e9ab97e344bd8e2db1ee1f7a7a9cf4
godv233 <zengwei233@126.com>
2020-03-09 10:29:44 +0800

    maven项目打包配置

---
+++ shop-common/src/main/java/com/swpu/shop/CommonApplication.java
@@ -0,0 +1,15 @@
+package com.swpu.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 曾伟 zengwei233@126.com
+ * @date 2020/3/9 8:15
+ */
+@SpringBootApplication
+public class CommonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CommonApplication.class, args);
+    }
+}
--- shop-flash/src/main/java/com/swpu/shop/shopflash/mapper/OrderDao.java
+++ shop-flash/src/main/java/com/swpu/shop/shopflash/mapper/OrderDao.java
@@ -1,5 +1,6 @@
 package com.swpu.shop.shopflash.mapper;
 
+import com.swpu.shop.entity.NewBeeMallOrder;
 import com.swpu.shop.shopflash.common.Result;
 import com.swpu.shop.shopflash.vo.FlashOrderDetailVo;
 import com.swpu.shop.shopflash.vo.FlashSaleOrder;
@@ -26,10 +27,10 @@ public interface OrderDao {
      * @param orderInfo
      * @return
      */
-//    @Insert("insert into order_info(user_id, goods_id, goods_name, goods_count, goods_price, order_channel, status, create_date)values("
-//            + "#{userId}, #{goodsId}, #{goodsName}, #{goodsCount}, #{goodsPrice}, #{orderChannel},#{status},#{createDate} )")
-//    @SelectKey(keyColumn = "id", keyProperty = "id", resultType = long.class, before = false, statement = "select last_insert_id()")
-//    long insert(NewBeeMallOrder orderInfo);
+    @Insert("insert into tb_newbee_mall_order(user_id, goods_id, goods_name, goods_count, goods_price, order_channel, status, create_date)values("
+            + "#{userId}, #{goodsId}, #{goodsName}, #{goodsCount}, #{goodsPrice}, #{orderChannel},#{status},#{createDate} )")
+    @SelectKey(keyColumn = "id", keyProperty = "id", resultType = long.class, before = false, statement = "select last_insert_id()")
+    long insert(NewBeeMallOrder orderInfo);
 
     /**
      * 插入秒杀订单
